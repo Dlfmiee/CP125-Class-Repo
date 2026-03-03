d5c32865fba36aea9c7c28fd02f04751617d0eab
Dlfmiee <aidilsahimat07@gmail.com>
2026-03-03 15:08:18 +0800

    Made a change

--- school_system/test.py
+++ school_system/test.py
@@ -1,4 +1,4 @@
-def count_students(input_file, output_file):
+"""def count_students(input_file, output_file):
 
     f = open(input_file, "r")
     
@@ -33,3 +33,73 @@ def check_low_attendance(input_file, output_file, threshold):
     f.close()
 
 check_low_attendance("school_system/students/2024/attendance.csv", "school_system/reports/statistics.txt", 75)
+
+import csv
+
+def filter_passing_students(config_file, scores_file, output_file):
+    
+    f = open(config_file, "r")
+    w = open(scores_file, "r",newline="")
+    
+    threshold = f.readline()
+    reader = csv.reader(w)
+    next(reader)
+    
+    listscore = []
+    
+    for line in reader:
+        if float(line[1]) >= float(threshold):
+            listscore.append(f"{line[0]}: {line[1]}")
+            
+    f.close()
+    w.close()
+    
+    f= open(output_file, "w",newline="")
+    for name in listscore:
+        f.write(name + "\n")
+    f.close()
+
+filter_passing_students("school_system/config/passing_grade.txt", "school_system/grades/midterm.csv", "school_system/reports/summary.txt")
+
+def calculate_averages(midterm_file, final_file, output_file):
+    
+    midterm_file = open(midterm_file, "r",newline="")
+    final_file = open(final_file, "r",newline="")
+    
+    midterm_reader = csv.reader(midterm_file)
+    final_reader = csv.reader(final_file)
+    
+    next(midterm_reader)
+    next(final_reader)
+    
+    averages = {}
+    
+    for line in midterm_reader:
+        name = line[0]
+        score = float(line[1])
+        averages[name] = [score]
+        
+    for line in final_reader:
+        name = line[0]
+        score = float(line[1])
+        if name in averages:
+            averages[name].append(score)
+        else:
+            averages[name] = [score]
+            
+    midterm_file.close()
+    final_file.close()
+    
+    f = open(output_file, "w",newline="")
+    
+    for name, scores in averages.items():
+        average_score = sum(scores) / len(scores)
+        f.write(f"{name}: {average_score:.1f}\n")
+    f.close()
+
+calculate_averages("school_system/grades/midterm.csv", "school_system/grades/final.csv", "school_system/reports/statistics.txt")"""
+
+def generate_enrollment_report(old_file, new_file, output_file):
+    
+
+generate_enrollment_report("school_system/students/2024/names.txt", "school_system/students/2025/enrollment.txt", "school_system/reports/summary.txt")
